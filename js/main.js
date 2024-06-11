1d57880581cd26793cb842ea088dacabcaf94a18
CliconlineStudios <105317102+CliconlineStudios@users.noreply.github.com>
2024-06-10 20:01:08 -0500

    nueva actualización de datos

--- js/main.js
+++ js/main.js
@@ -160,12 +160,6 @@ function sharetj() {
 
 //   boton foto
 
-document.querySelector('.slider')   
-.addEventListener('click',function(){
-    planes();
-    
-
-});
 
 //   boton atras
 
@@ -256,51 +250,6 @@ function back() {
             
         }
 
-  // slider
- 
-  const slider = document.querySelector("#slider");
-  let sliderSection = document.querySelectorAll(".slider__section");
-  let sliderSectionlast = sliderSection[sliderSection.length -1];
-  
-  const btnLeft = document.querySelector("#btn-left");
-  const btnRight = document.querySelector("#btn-rigth");
-  
-  slider.insertAdjacentElement('afterbegin', sliderSectionlast);
-  
-  function next (){
-      let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
-      slider.style.marginLeft = "-200%";
-      slider.style.transition = "all 0.5s";
-      setTimeout(function(){
-          slider.style.transition = "none";
-          slider.insertAdjacentElement('beforeend', sliderSectionFirst);
-          slider.style.marginLeft = "-100%";
-      }, 500);  
-  }
-  
-  function Prev (){
-      let sliderSection = document.querySelectorAll(".slider__section");
-      let sliderSectionLast = sliderSection[sliderSection.length -1];
-      slider.style.marginLeft = "0";
-      slider.style.transition = "all 0.5s";
-      setTimeout(function(){
-          slider.style.transition = "none";
-          slider.insertAdjacentElement('afterbegin', sliderSectionLast);
-          slider.style.marginLeft = "-100%";
-      }, 500);  
-  }
-  
-  btnRight.addEventListener('click', function(){
-      next();
-  });
-  
-  btnLeft.addEventListener('click', function(){
-      Prev();
-  });
-  
-  setInterval(function(){
-      next();
-  },3000);   
   
 
 // llevar arriba
